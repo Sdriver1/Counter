ba82669deb5c60634e30043cdee5bf7f7eb65800
Sdriver1 <sjdriver08@gmail.com>
2025-12-23 19:48:06 -0500

    Updating web

--- web/server.js
+++ web/server.js
@@ -1,17 +1,23 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/invite", (req, res) => {
+  res.redirect(
+    "https://discord.com/oauth2/authorize?client_id=1453159969103810752&permissions=6755794578369616&integration_type=0&scope=bot"
+  );
 });
 
 app.listen(PORT, () => {
-    console.log(`📚 Documentation server running at http://localhost:${PORT}`);
+  console.log(`📚 Documentation server running at http://localhost:${PORT}`);
 });
 
 module.exports = app;
